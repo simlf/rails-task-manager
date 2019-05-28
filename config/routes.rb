977bd21f8b25a825cc5c2e9f2c2a7018e9f50a60
Simon Le Fourn <lefournsimon@gmail.com>
2019-05-28 13:03:11 +0200

    added CR

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # Create a task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # Read one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
